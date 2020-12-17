3ddcc8921a90e8e29cf183d5abdf2caf58edd6d4
ahmedfouda <ahmedfouda04@gamil.com>
2020-12-17 17:13:16 +0200

    seconde commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// ------Loader-----//
 $(document).ready(function() {
     $(".loader").delay(1000).fadeOut("slow");
+});
+
+// -----Date Time Picker----- //
+$('.datepicker').datepicker();
+//-----//
+$(document).ready(function() {
+    $('select').niceSelect();
 });
